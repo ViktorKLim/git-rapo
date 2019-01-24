10604e303ead7e03106760c81dd6abdeb87a5794
ViktorKlim <viktor.klin00@gmail.com>
2019-01-24 11:48:30 +0600

    Поставил новую точку с запятой

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Всем привет!;");
+            Console.WriteLine("Всем привет!;;");
 
         }
     }
